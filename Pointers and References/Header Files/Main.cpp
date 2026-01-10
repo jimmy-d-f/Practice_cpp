f7b938a1dfb5a8faebfb9208161b28bf0b09c381
jimmy <jimmyfrosting@gmail.com>
2026-01-10 07:29:56 +0000

    Remove Pointers and References from remote

--- Pointers and References/Header Files/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Printn.h"
-using namespace std;
-
-int main() {
-    Printn("Hello, World!");
-    return 0;
-}
--- Pointers and References/Header Files/Printn.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "Printn.h"
-using namespace std;
-
-void Printn(const char* str) {
-    cout << str << endl;
-}
