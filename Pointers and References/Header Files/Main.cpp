6ac5adeb8dcf61ced6136b2b51c09a608aad42df
jimmy <jimmyfrosting@gmail.com>
2026-01-10 07:10:45 +0000

    Add project folders

---
+++ Pointers and References/Header Files/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Printn.h"
+using namespace std;
+
+int main() {
+    Printn("Hello, World!");
+    return 0;
+}
---
+++ Pointers and References/Header Files/Printn.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void Printn(const char* message);
